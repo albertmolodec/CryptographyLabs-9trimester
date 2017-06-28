209811caff8ef9c4c26187118c200d214a0615b6
albertmolodec <albert.abdu@gmail.com>
2017-06-29 04:35:09 +0500

    Complete Hash and SHA256

--- 7_Hash/7_Hash.cpp
+++ 7_Hash/7_Hash.cpp
@@ -1,40 +1,59 @@
 #include "MyFile.h"
-#include "..\third-party\src\CryptoPP\cryptlib.h"
-#include "..\third-party\src\CryptoPP\rsa.h"
-#include "..\third-party\src\CryptoPP\modes.h"
-#include "..\third-party\src\CryptoPP\osrng.h"
-#include "..\third-party\src\CryptoPP\filters.h"
-#include "..\third-party\src\CryptoPP\base64.h"
-
-
 
 class Hash
 {
 private:
 	MyFile plaintext;
-	MyFile ciphertext;
-	MyFile decryptedtext;
-	MyFile publickey;
-	MyFile privatekey;
-	std::string plaintext_path = "../shared/plaintext.doc";
-	std::string ciphertext_path = "../shared/ciphertext.doc";
-	std::string decryptedtext_path = "../shared/decryptedtext.doc";
-	std::string publickey_path = "../shared/publickey";
-	std::string privatekey_path = "../shared/privatekey";
+	MyFile hash;
+	std::string plaintext_path = "../shared/plaintext.docx";
+	std::string hash_path = "../shared/hash.txt";
 public:
-	Hash()
+	void Generate_Hash()
 	{
+		int hash_size = 256;
+		int plain_size;
+		std::vector<unsigned char> iv;
+		
+		std::cout << "Hash is generating..." << std::endl;
+		Open_Message(plaintext, plaintext_path);
+		plain_size = plaintext.GetData().size();
 
-	}
+		for (int i = 0; i < hash_size; i++)
+		{
+			unsigned char perem = i << 2;
+			int plain_index = i % plain_size;
+			iv.push_back(plaintext.GetData()[plain_index] * perem + 17);
+		}
 
-	
+		for (int i = 0; i < hash_size; i++)
+		{
+			hash.GetData().push_back((iv[i]) ^ (i / 2));
+		}
+		
+		if (plain_size > hash_size)
+		{
+			for (int i = 0; i < plain_size; i++)
+			{
+				hash.GetData()[i % hash_size] ^= plaintext.GetData()[i];
+			}
+		}
+		else
+		{
+			for (int i = 0; i < hash_size; i++)
+			{
+				hash.GetData()[i] ^= plaintext.GetData()[i % plain_size];
+			}
+		}		
+
+		Clear_Screen();
+		Write_Message(hash, hash_path, "Hash file");
+	}	
 };
 
 int main()
 {
-	
-
-
+	Hash obj;
+	obj.Generate_Hash();
 	system("pause");
 	return 0;
 }
--- 8_SHA256/8_SHA256.cpp
+++ 8_SHA256/8_SHA256.cpp
@@ -1,30 +1,45 @@
 #include "MyFile.h"
 #include "..\third-party\src\CryptoPP\cryptlib.h"
-#include "..\third-party\src\CryptoPP\rsa.h"
-#include "..\third-party\src\CryptoPP\modes.h"
+#include "..\third-party\src\CryptoPP\sha.h"
 #include "..\third-party\src\CryptoPP\osrng.h"
-#include "..\third-party\src\CryptoPP\filters.h"
+#include "..\third-party\src\CryptoPP\hex.h"
 #include "..\third-party\src\CryptoPP\base64.h"
-
+#include <iomanip>
 
 
 class SHA256
 {
 private:
 	MyFile plaintext;
-	MyFile ciphertext;
-	MyFile decryptedtext;
-	MyFile publickey;
-	MyFile privatekey;
+	MyFile hash;
 	std::string plaintext_path = "../shared/plaintext.docx";
-	std::string ciphertext_path = "../shared/ciphertext.docx";
-	std::string decryptedtext_path = "../shared/decryptedtext.doc";
-	std::string publickey_path = "../shared/publickey";
-	std::string privatekey_path = "../shared/privatekey";
+	std::string hash_path = "../shared/hash_SHA256.txt";
 public:
-	SHA256()
+	void Generate_Hash_SHA256()
 	{
+		std::cout << "Hash is generating..." << std::endl;
+		Open_Message(plaintext, plaintext_path);
 
+		int plain_size = plaintext.GetData().size();
+		std::string str_plain = "";
+		std::string str_hash = "";
+
+		for (int i = 0; i < plain_size; i++)
+		{
+			str_plain += plaintext.GetData()[i];
+		}
+
+		CryptoPP::SHA256 sha256;
+		CryptoPP::StringSource(str_plain, true, new CryptoPP::HashFilter(sha256, new CryptoPP::HexEncoder(new CryptoPP::StringSink(str_hash))));
+
+		for (int i = 0; i < str_hash.size(); i += 2)
+		{
+			unsigned char tmp = ((unsigned char)str_hash[i] << 4) + (unsigned char)str_hash[i + 1];
+			hash.GetData().push_back(tmp);
+		}
+
+		Clear_Screen();
+		Write_Message(hash, hash_path, "SHA256 file");
 	}
 
 
@@ -32,9 +47,8 @@ public:
 
 int main()
 {
-
-
-
+	SHA256 obj;
+	obj.Generate_Hash_SHA256();
 	system("pause");
 	return 0;
 }
